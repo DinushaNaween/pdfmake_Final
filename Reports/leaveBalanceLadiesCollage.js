d71ef1df43c1b15fd3584f6f2b43c8cad2006691
dldnd <dinusha.aspitio@gmail.com>
2019-09-23 11:40:27 +0530

    staff details report done

---
+++ Reports/leaveBalanceLadiesCollage.js
@@ -0,0 +1,151 @@
+var fetch = require('node-fetch');
+var url = require('../url');
+var pdfmake = require('../js/index');
+
+var fonts = {
+  Roboto: {
+    normal: 'fonts/Roboto-Regular.ttf',
+    bold: 'fonts/Roboto-Medium.ttf',
+    italics: 'fonts/Roboto-Italic.ttf',
+    bolditalics: 'fonts/Roboto-MediumItalic.ttf'
+  },
+  Tinos: {
+    normal: 'fonts/Tinos-Regular.ttf',
+    bold: 'fonts/Tinos-Bold.ttf',
+    italics: 'fonts/Tinos-Italic.ttf',
+    bolditalics: 'fonts/Tinos-BoldItalic.ttf'
+  }
+};
+
+var pdfmake = require('../js/index');
+pdfmake.setFonts(fonts);
+
+const getStaffData = async url => {
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    return (json.data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+var schoolName = 'Staff';
+var reportType = 'Information';
+var reportSubType = 'Staff Details';
+var dueDate = '20/09/2019'
+
+async function generateReport() {
+  const reportData = await getStaffData(url.allStaff);
+  // console.log(reportData);
+
+  function buildTableBody(data){
+    var body = [];
+
+    body.push([{ text: 'Staff ID', style: 'tableHeader' }, { text: 'Full Name', style: 'tableHeader' }, { text: 'Section', style: 'tableHeader' }, { text: 'Designation', style: 'tableHeader' }, { text: 'E-Mail', style: 'tableHeader' }, { text: 'Gender', style: 'tableHeader' }])
+
+    for(var i=0; i<data.length; i++){
+      var dataRow = [];
+
+      dataRow.push(data[i].staffId);
+      dataRow.push(data[i].fullName);
+      dataRow.push(data[i].section);
+      dataRow.push(data[i].designation);
+      dataRow.push(data[i].email);
+      dataRow.push(data[i].gender);
+
+      body.push(dataRow);
+    }
+
+    return body;
+  }
+
+  function table(data){
+    return {
+      table: {
+        widths: ['6%', '43%', '15%', '10%', '20%', '10%'],
+        headerRows: 1,
+        body: buildTableBody(data)
+      }, 
+        fontSize: 9, 
+        absolutePosition: { x: 18, y: 90 }
+    }
+  }
+
+  var dd = {
+    footer: function (currentPage, pageCount) {
+      return {
+        margin: 10,
+        columns: [
+          {
+            text: 'Date: 20/09/2019    Time: 9.45 AM',
+            style: 'planText',
+            alignment: 'left'
+          },
+          {
+            text: 'Page ' + currentPage.toString() + ' of ' + pageCount,
+            style: 'planText',
+            alignment: 'right'
+          }
+        ]
+      };
+    },
+    content: [
+      {
+        canvas: [
+          {
+            type: 'line',
+            x1: 40, y1: 60,
+            x2: 530, y2: 60,
+            lineWidth: 0.5
+          }
+        ], absolutePosition: { x: 34, y: 15 }
+      },
+      {
+        image: '../Reports/Images/Capture.PNG',
+        width: 50,
+        absolutePosition: { x: 13, y: 25 }
+      },
+      {
+        text: reportType + ' - ' + reportSubType,
+        style: 'header',
+        absolutePosition: { x: 78, y: 32 }
+      },
+      {
+        text: schoolName,
+        style: 'subHeader',
+        absolutePosition: { x: 78, y: 55 }
+      },
+      {
+        text: 'As At: ' + dueDate,
+        style: 'planText',
+        absolutePosition: { x: 465, y: 60 }
+      },
+      table(reportData),
+    ],
+    styles: {
+      header: {
+        fontSize: 14
+      },
+      subHeader: {
+        fontSize: 12
+      },
+      tableHeader: {
+        fontSize: 11
+      },
+      planText: {
+        fontSize: 10
+      }
+    }
+
+  }
+
+  var now = new Date();
+
+  var pdf = pdfmake.createPdf(dd);
+  pdf.write('pdfs/LC.pdf');
+
+  console.log(new Date() - now);
+};
+
+generateReport();
