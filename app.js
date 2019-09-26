0092945189529ccb5060165d53c42a5d5bba55cd
wishwaprabodha <ww46673@gmail.com>
2019-09-26 11:00:49 +0530

    wishwa

--- app.js
+++ app.js
@@ -7,35 +7,20 @@ var testRoutes = require('./Reports/test/testServer');
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
+app.use((req, res) => {
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE')
     return res.status(200).json({});
   }
-  next();
 });
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
   console.log("Hello ClzMate");
   res.status(200).json({
     state: true
-  })
-})
-
-app.use('/lc', testRoutes);
-
-app.use((req, res, next) => {
-  const error = new Error('Not Found');
-  error.status(404);
-  next(error);
+  });
 });
 
-app.use((error, req, res, next) => {
+app.use((error, req, res) => {
   res.status(error.status || 500);
   res.json({
     error: {
@@ -43,5 +28,8 @@ app.use((error, req, res, next) => {
     }
   });
 });
+
+app.use('/lc', testRoutes);
+
 
 module.exports = app;
